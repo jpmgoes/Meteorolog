a5926025d17186efbc33275acc7fae898559b8a0
JPMouraG <jp.mouragoes@gmail.com>
2021-03-04 08:54:46 -0300

    exemplo forecast component created

---
+++ src/components/Exemplos.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Exemplos = () => {
+  return (
+    <div className="ex-forecast">
+      <h3 className="ex-forecast__title">Weather Forecast</h3>
+    </div>
+  );
+};
