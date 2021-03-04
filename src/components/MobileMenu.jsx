e179485be8bcb029b65ea3a5a863ab55c0be4c69
JPMouraG <jp.mouragoes@gmail.com>
2021-03-04 10:29:44 -0300

    sidebar mobile updated

---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const MobileMenu = ({ open }) => {
+  return (
+    <>
+      <div className="mobile-block"></div>
+      <div className="mobile-menu">
+        <ul>
+          <li>teste</li>
+          <li>teste</li>
+          <li>teste</li>
+        </ul>
+      </div>
+    </>
+  );
+};
