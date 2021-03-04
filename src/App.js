aab82838928bc6af557e3cb573c998cfffacd5fb
JPMouraG <jp.mouragoes@gmail.com>
2021-03-04 01:33:09 -0300

    commited

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { Card } from "./components/Card";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 import "./App.scss";
+import { Background } from "./components/Background";
 
 function App() {
-  return <div className="App"></div>;
+  const { handleSubmit, register } = useForm();
+  const [input, setInput] = useState("");
+  const onSubmit = handleSubmit((data) => {
+    setInput(data.location);
+    return data;
+  });
+
+  return (
+    <div className="App">
+      <Background />
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="location"
+          name="location"
+          ref={register({ required: true })}
+        />
+        <input type="submit" value="submit" />
+      </form>
+      <Card input={input} />
+    </div>
+  );
 }
 
 export default App;
