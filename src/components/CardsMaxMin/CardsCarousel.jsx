52c2b5c5b2ac0dc123171304c866d110f3fed749
JPMouraG <jp.mouragoes@gmail.com>
2021-03-06 02:51:44 -0300

    card max and min updated

---
+++ src/components/CardsMaxMin/CardsCarousel.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Carousel } from "../../layout/carousel/Carousel";
+import { CardsCarouselContent } from "./CardsCarouselContent";
+
+export const CardsCarousel = ({ name }) => {
+  return (
+    <div>
+      <h3>{name}</h3>
+      <Carousel>
+        <CardsCarouselContent />
+      </Carousel>
+    </div>
+  );
+};
