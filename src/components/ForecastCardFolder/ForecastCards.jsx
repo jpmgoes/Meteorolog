a35b05831e9de7fb8b1246b72e2a306bc87136a9
JPMouraG <jp.mouragoes@gmail.com>
2021-03-10 01:02:54 -0300

    context created

--- src/components/ForecastCardFolder/ForecastCards.jsx
+++ src/components/ForecastCardFolder/ForecastCards.jsx
@@ -1,21 +1,14 @@
-import { useContext } from "react";
-import { LocationInfoContext } from "../../App";
 import { Carousel } from "../../layout/carousel/Carousel";
 
 import { ForecastCardsContent } from "./ForecastCardsContent";
 import "./style/style.scss";
 
 export const ForecastCards = () => {
-  const { setName, setLocation, setCountry } = useContext(LocationInfoContext);
   return (
     <div>
       <h3> Weather Forecast </h3>
       <Carousel contentName="forecast" styleName="carousel">
-        <ForecastCardsContent
-          setName={setName}
-          setLocation={setLocation}
-          setCountry={setCountry}
-        />
+        <ForecastCardsContent />
       </Carousel>
     </div>
   );
