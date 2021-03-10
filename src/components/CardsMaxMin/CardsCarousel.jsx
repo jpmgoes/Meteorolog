530ecd9f17362d10a0e4519f085992eeba73b71b
JPMouraG <jp.mouragoes@gmail.com>
2021-03-10 00:56:09 -0300

    before

--- src/components/CardsMaxMin/CardsCarousel.jsx
+++ src/components/CardsMaxMin/CardsCarousel.jsx
@@ -1,10 +1,8 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Carousel } from "../../layout/carousel/Carousel";
 import { CardsCarouselContent } from "./CardsCarouselContent";
-import { LocationInfoContext } from "../../App";
 
-export const CardsCarousel = () => {
-  const { name } = useContext(LocationInfoContext);
+export const CardsCarousel = ({ name, data }) => {
   return (
     <div>
       <h3>{name}</h3>
@@ -12,7 +10,7 @@ export const CardsCarousel = () => {
         contentName="cards-carousel"
         styleName="carousel carousel__cards-carousel__style"
       >
-        <CardsCarouselContent />
+        <CardsCarouselContent data={data} />
       </Carousel>
     </div>
   );
