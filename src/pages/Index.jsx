4d1df6b26b8cebd0998f83b789ebc0d28a212fac
JPMouraG <jp.mouragoes@gmail.com>
2021-03-10 00:45:36 -0300

    before

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,21 +1,51 @@
+import { useContext } from "react";
+
 import { Layout } from "../layout/Layout";
 import { ForecastCards } from "../components/ForecastCardFolder/ForecastCards";
 import { Form } from "../components/Form";
 import { CardsCarousel } from "../components/CardsMaxMin/CardsCarousel";
 import { MyLocation } from "../components/MyLocation";
+import { LocationInfoContext } from "../App";
 
 const Index = () => {
+  const {
+    setName,
+    name,
+    setCountry,
+    country,
+    setDataToSideCard,
+    dataToSideCard,
+    setInput,
+    setLocation,
+    dataToCardsCarousel,
+    systemPattern,
+  } = useContext(LocationInfoContext);
+
   return (
-    <Layout queryPag={"navbar__dashboard"}>
+    <Layout
+      queryPag={"navbar__dashboard"}
+      dataToSideCard={dataToSideCard}
+      country={country}
+      name={name}
+      systemPattern={systemPattern}
+    >
       <div className="main__square__right__search">
-        <Form />
-        <MyLocation />
+        <Form setInput={setInput} />
+        <MyLocation
+          setName={setName}
+          setLocation={setLocation}
+          setCountry={setCountry}
+        />
       </div>
       <div className="main__square__right__weather-forecast">
-        <ForecastCards />
+        <ForecastCards
+          setName={setName}
+          setLocation={setLocation}
+          setCountry={setCountry}
+        />
       </div>
       <div className="main__square__right__cards">
-        <CardsCarousel />
+        <CardsCarousel name={name} data={dataToCardsCarousel} />
       </div>
       <div className="main__square__right__weather-graph"></div>
     </Layout>
