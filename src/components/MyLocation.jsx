e22d8f883671b112ee3550cfb6ba272667229228
JPMouraG <jp.mouragoes@gmail.com>
2021-03-10 00:55:07 -0300

    before

--- src/components/MyLocation.jsx
+++ src/components/MyLocation.jsx
@@ -1,9 +1,6 @@
-import { useContext } from "react";
 import { getNameByLatAndLon } from "../api/getLocation";
-import { LocationInfoContext } from "../App";
 
-export const MyLocation = () => {
-  const { setLocation, setName, setCountry } = useContext(LocationInfoContext);
+export const MyLocation = ({ setLocation, setName }) => {
   function getCurrentlyLocation() {
     navigator.geolocation.getCurrentPosition((location) => {
       const lat = location.coords.latitude;
@@ -11,8 +8,6 @@ export const MyLocation = () => {
       setLocation([lat, lon]);
       getNameByLatAndLon(lat, lon).then((data) => {
         const name = data["results"][0]["components"]["city"];
-        const country = data["results"][0]["components"]["country"];
-        setCountry(country);
         setName(name);
       });
     });
